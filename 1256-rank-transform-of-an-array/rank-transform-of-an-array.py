82c50f1721e92ac2975ae7102c6cf49b9858ebd6
Kajal Kumari <163738511+2300030778@users.noreply.github.com>
2026-07-12 21:27:02 +0530

    Time: 36 ms (66.70%) | Memory: 37.6 MB (60.61%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+
+        unique = sorted(set(arr))
+
+        rank = {}
+
+        for i, num in enumerate(unique):
+            rank[num] = i + 1
+        return [rank[num] for num in arr]    
+
+
+        
